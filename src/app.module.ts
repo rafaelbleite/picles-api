75e00ec10db35e1a77310062924d4a5524f592d2
RafaelBLeite <rafaelleite.dev@gmail.com>
2024-04-05 20:14:08 -0300

    feat: add config dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShelterModule } from './shelter/shelter.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('DB_CONNECTION_STRING'),
+      }),
+    }),
     ShelterModule,
-    MongooseModule.forRoot('')],
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
