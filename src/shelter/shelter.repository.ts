1d51a7a4e253fa7d352389996224e41eda67d150
RafaelBLeite <rafaelleite.dev@gmail.com>
2024-04-09 19:51:38 -0300

    fix(shelter): alter export definition of class ShelterRepository

--- src/shelter/shelter.repository.ts
+++ src/shelter/shelter.repository.ts
@@ -5,7 +5,7 @@ import IShelterRepository from "./interfaces/shelter.repository.interface";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class ShelterRepository implements IShelterRepository {
+export default class ShelterRepository implements IShelterRepository {
   constructor(
     @InjectModel(Shelter.name)
     private readonly ShelterModel: Model<Shelter>
